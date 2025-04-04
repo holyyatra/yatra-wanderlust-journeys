7926e3596e302445a5c3469eea44bca592c52cbe
holyyatra <guptaasish842@gmail.com>
2025-04-04 07:44:18 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,12 +12,13 @@ const Header = () => {
       <div className="container mx-auto px-4 py-4">
         <div className="flex items-center justify-between">
           <Link to="/" className="flex items-center space-x-2">
-            <div className="w-10 h-10 bg-yatra-saffron rounded-md flex items-center justify-center">
-              <span className="text-white font-bold text-xl">Y</span>
-            </div>
-            <span className="text-xl md:text-2xl font-bold text-yatra-blue">
-              Holy<span className="text-yatra-saffron">Yatra</span>
-            </span>
+            <Image
+              src="https://github.com/holyyatra/holyatraweb/blob/main/logohy.png?raw=true"
+              alt="Holy Yatra Logo"
+              width={150}
+              height={40}
+              className="h-10 w-auto"
+            />
           </Link>
 
           {/* Desktop Navigation */}
